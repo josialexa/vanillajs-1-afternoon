c5735b3c90a482d280fbd4dedf55cea015944f89
Josi Reis-West <josialexa85@gmail.com>
2019-11-15 15:49:49 -0600

    Completed Part 1 step 4

--- Part1/index.js
+++ Part1/index.js
@@ -1,18 +1,13 @@
-console.log('hello');
+// console.log('hello');
+let gameArr = [];
 
 const play = (boxID) => {
     let curPlayerSpan = document.getElementById('player');
     let curPlayer = curPlayerSpan.innerText;
-    console.log(curPlayer);
     let clickedBox = document.getElementById(boxID);
     
     clickedBox.innerText = curPlayer;
+    gameArr[boxID] = curPlayer;
+    console.log(gameArr);
     curPlayerSpan.innerText = curPlayer == 'X' ? 'O' : 'X';
-    // if(curPlayer == 'X') {
-    //     console.log('its X');
-    //     curPlayerSpan.innerText = 'O';
-    // } else {
-    //     console.log('its not X');
-    //     curPlayerSpan.innerText = 'X';
-    // }
 };
