fc26d194f1ffaaecea63ec16aab79dc4e3222672
Josi Reis-West <josialexa85@gmail.com>
2019-11-17 09:57:48 -0600

    Completed step 5

--- Part1/index.js
+++ Part1/index.js
@@ -8,6 +8,55 @@ const play = (boxID) => {
     
     clickedBox.innerText = curPlayer;
     gameArr[boxID] = curPlayer;
-    console.log(gameArr);
+
+    //check for winner
+    //check horizontal
+    for(let i = j = Math.floor(boxID / 3) * 3; i < j + 3; i++) {
+        if(gameArr[i] != curPlayer) {
+            break;
+        } else if(i - j >= 2) {
+            //declare winner as curPlayer
+            winner(curPlayer);
+        }
+    }
+    
+    //check vertical
+    for(let i = j = boxID % 3; i <= j + 6; i += 3) {
+        if(gameArr[i] != curPlayer) {
+            break;
+        } else if(i - j == 6) {
+            //declare winner as curPlayer
+            winner(curPlayer);
+        }
+    }
+    
+    //check diag
+    for(let i = 0; i < 9; i += 4) {
+        if(gameArr[i] != curPlayer) {
+            break;
+        } else if(i == 8) {
+            //declare winner as curPlayer
+            winner(curPlayer);
+        }
+    }
+    
+    //check reverse diag
+    for(let i = 2; i <= 6; i += 2) {
+        if(gameArr[i] != curPlayer) {
+            break;
+        } else if(i >= 6) {
+            //declare winner as curPlayer
+            winner(curPlayer);
+        }
+    }
+
+    if(gameArr.findIndex(e => e == undefined) == -1 && gameArr.length == 9) {
+        alert("CAT's game!");
+    }
+
     curPlayerSpan.innerText = curPlayer == 'X' ? 'O' : 'X';
 };
+
+let winner = (winner) => {
+    alert(`${winner} wins!`);
+}
