48b1a1fb3f3a5deaa24f3ffcb4cee11e111883bd
Josi Reis-West <josialexa85@gmail.com>
2019-11-18 09:19:19 -0600

    Completed part 2 step 3

--- Part2/index.js
+++ Part2/index.js
@@ -1,5 +1,7 @@
 //console.log('The house always wins!');
-let cardID = document.getElementById('cardID').value;
-let cardStyle = document.getElementById('cardStyle').value;
 
-console.log(cardID, cardStyle);
+const setCard = () => {
+    let cardID = document.getElementById('cardID').value;
+    let cardStyle = document.getElementById('cardStyle').value;
+    console.log(cardID, cardStyle);
+};
