902c5819e15239d8055fc700b1f16e4dd934faa1
Josi Reis-West <josialexa85@gmail.com>
2019-11-18 10:02:25 -0600

    Completed part 2 step 4 and Black Diamond

--- Part2/index.js
+++ Part2/index.js
@@ -3,5 +3,16 @@
 const setCard = () => {
     let cardID = document.getElementById('cardID').value;
     let cardStyle = document.getElementById('cardStyle').value;
-    console.log(cardID, cardStyle);
+    // console.log(cardID, cardStyle);
+    document.getElementById(cardID).style.color = cardStyle;
 };
+
+const reset = () => {
+    let cards = document.getElementsByTagName('section');
+
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].style.color = "grey";
+    }
+}
+
+document.getElementById('resetCards').addEventListener('click', reset);
